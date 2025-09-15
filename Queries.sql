51f4039db69f3935962a435387dc6f9825d30bf5
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-15 22:10:00 +0530

    answer for question 4

--- Queries.sql
+++ Queries.sql
@@ -19,3 +19,14 @@ JOIN taxonomy t ON r.ncbi_id = t.ncbi_id
 WHERE t.species LIKE '%Oryza%'
 ORDER BY r.length DESC
 LIMIT 1;
+
+-- query to find paginate list of the family names and their longest DNA sequence lengths (in descending order of length) where only    families that have DNA sequence lengths greater than 1,000,000 are included.
+
+SELECT f.rfam_acc AS family_acc, f.description AS family_name, MAX(r.length) AS max_length
+FROM family f
+JOIN full_region fr ON f.rfam_acc = fr.rfam_acc
+JOIN rfamseq r ON fr.rfamseq_acc = r.rfamseq_acc
+GROUP BY f.rfam_acc, f.description
+HAVING MAX(r.length) > 1000000
+ORDER BY max_length DESC
+LIMIT 15 OFFSET 120;
